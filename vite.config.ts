4c24ba4344c6c5fca6c5ea38e69722a1ba6d120e
ari3210-dev <ari3210@guru.smk.belajar.id>
2026-06-11 21:08:34 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Trik memaksa Vite mengabaikan file data kuis/glossary yang hilang
+    rollupOptions: {
+      external: ['./data/quiz', './data/glossary', './types'],
+    }
+  }
 })
